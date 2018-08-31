4c71f38659b2c8a4f3ae42d98acde6d46a3250c9
Yuichiro Sawai <sawai.yuichiro.sn0@is.naist.jp>
2018-08-31 23:38:00 +0900

    Process per page

--- src/main/java/com/github/yuichiro_s/App.java
+++ src/main/java/com/github/yuichiro_s/App.java
@@ -10,12 +10,10 @@ public class App
         CommandLineParser parser = new DefaultParser();
         try {
             Options options = new Options();
-            options.addOption("dump", true, "Wiktionary dump");
             options.addOption("lang", true, "language to extract");
             CommandLine line = parser.parse(options, args);
             Language lang = Language.valueOf(line.getOptionValue("lang"));
-            String dumpPath = line.getOptionValue("dump");
-            new Extractor(dumpPath, lang).extract();
+            new Extractor(lang).extract();
         }
         catch(ParseException exp) {
             System.err.println("Parsing failed.  Reason: " + exp.getMessage());
--- src/main/java/com/github/yuichiro_s/Extractor.java
+++ src/main/java/com/github/yuichiro_s/Extractor.java
@@ -1,16 +1,73 @@
 package com.github.yuichiro_s;
 
-public class Extractor {
-    private final String dumpPath;
-    private final Language lang;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-    public Extractor(String dumpPath, Language lang) {
-        this.dumpPath = dumpPath;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
+
+class Extractor extends DefaultHandler {
+    private final Language lang;
+    private final StringBuilder builder;
+    private String currentTitle;
+
+    Extractor(Language lang) {
         this.lang = lang;
+        this.builder = new StringBuilder();
+        this.currentTitle = null;
     }
 
-    public void extract() {
-        System.out.println(this.dumpPath);
-        System.out.println(this.lang);
+    void extract() {
+        try {
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+            Reader reader = new InputStreamReader(new BufferedInputStream(System.in), "UTF-8");
+            InputSource is = new InputSource(reader);
+            is.setEncoding("UTF-8");
+            parser.parse(is, this);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        builder.setLength(0);
+    }
+
+    @Override
+    public void characters(char ch[], int start, int length) {
+        builder.append(ch, start, length);
+    }
+
+    @Override
+    public void endElement(String uri, String localname, String qName) {
+        String s = builder.toString();
+        process(qName, s);
+    }
+
+    private void process(String tag, String text) {
+        if (tag.equals("title")) {
+            currentTitle = text;
+        } else if (tag.equals("text")) {
+            processPage(currentTitle, text);
+        }
+    }
+
+    private void processPage(String title, String text) {
+        System.out.print("=================");
+        System.out.print(title);
+        System.out.print("=================");
+        System.out.println();
+        System.out.println(text);
     }
 }
